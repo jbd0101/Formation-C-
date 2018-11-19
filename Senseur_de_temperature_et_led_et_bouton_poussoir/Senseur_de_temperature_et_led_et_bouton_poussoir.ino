12af4005db7eef74e3d655be0401ec07b6d309a4
jean-christophe bauduin <maildejc@hotmail.be>
2018-11-19 17:03:24 +0100

    decoupage en fonctions

--- Senseur_de_temperature_et_led_et_bouton_poussoir/Senseur_de_temperature_et_led_et_bouton_poussoir.ino
+++ Senseur_de_temperature_et_led_et_bouton_poussoir/Senseur_de_temperature_et_led_et_bouton_poussoir.ino
@@ -1,78 +1,99 @@
-/*    Signal lumineux en fonction de la température et 
- *     mesure immédiate à l'aide du bouton poussoir 
- *     
+/*    Signal lumineux en fonction de la température et
+ *     mesure immédiate à l'aide du bouton poussoir
+ *
   */
 
-//     Initialisation des variables pour le temps qui s'écoule 
-int ledPinRouge = 13;   //LED  rouge en broche 13
-int ledPinOrange = 12;   //LED  orange en broche 12
-int ledPinVert = 11;     // LED vert en broche 11
-int ledPinButton = 10;  //LED de bouton-poussoir jaune en broche 10
-int buttonPin = 8;      //Bouton-poussoir en broche 8
+//     Initialisation des variables pour le temps qui s'écoule
+const int ledPinRouge = 13;   //LED  rouge en broche 13
+const int ledPinOrange = 12;   //LED  orange en broche 12
+const int ledPinVert = 11;     // LED vert en broche 11
+const int ledPinButton = 10;  //LED de bouton-poussoir jaune en broche 10
+const int buttonPin = 8;      //Bouton-poussoir en broche 8
+const int interval = 50;    //Intervalle de temps (1 secondes)
+const int sensorPin = 0;
 int buttonState;        //Variable pour enregistrement de l’état du bouton-poussoir
-int interval = 50;    //Intervalle de temps (1 secondes)
-unsigned long prev;     //Variable de temps
-int ledState = LOW;     //Variable d’état pour la LED clignotante 
-
-int sensorPin = 0; 
+int ledState = LOW;     //Variable d’état pour la LED clignotante
 float temperatureC;
+unsigned long prev;     //Variable de temps
+
+
+void setOrange(){
+  Serial.println("Couleur orange sélectionné");
+  digitalWrite (ledPinRouge,LOW);
+  digitalWrite (ledPinOrange,HIGH);
+  digitalWrite (ledPinVert,LOW);
+}
+void setRed(){
+  Serial.println("Couleur rouge sélectionné");
+  digitalWrite (ledPinRouge,HIGH);
+  digitalWrite (ledPinOrange,LOW);
+  digitalWrite (ledPinVert,LOW);
+}
+void setVert(){
+  Serial.println("Couleur verte sélectionné");
+  digitalWrite (ledPinRouge,LOW);
+  digitalWrite (ledPinOrange,LOW);
+  digitalWrite (ledPinVert,HIGH);
+}
+void setWhite(){
+  Serial.println("Couleur blanche sélectionné");
+  digitalWrite (ledPinRouge,LOW);
+  digitalWrite (ledPinOrange,LOW);
+  digitalWrite (ledPinVert,LOW);
+}
+
 
 void setup()
 {
   Serial.begin (9600);
   pinMode(ledPinRouge, OUTPUT);  //Broche LED clignotante comme sortie   
   pinMode(ledPinOrange, OUTPUT);  //Broche LED clignotante comme sortie   
-  pinMode(ledPinVert, OUTPUT);  //Broche LED clignotante comme sortie 
-  digitalWrite (ledPinRouge,LOW);
-  digitalWrite (ledPinOrange,LOW);
-  digitalWrite (ledPinVert,LOW);
-  
-  pinMode(ledPinButton, OUTPUT); //Broche LED de bouton-poussoir comme sortie   
-  pinMode (buttonPin, INPUT);    //Broche bouton-poussoir comme entrée   
-  prev = millis();               //Mémoriser le compteur de temps actuel
-} 
+  pinMode(ledPinVert, OUTPUT);  //Broche LED clignotante comme sortie
+  setWhite();
 
-void loop()                     
+  pinMode(ledPinButton, OUTPUT); //Broche LED de bouton-poussoir comme sortie
+  pinMode (buttonPin, INPUT);    //Broche bouton-poussoir comme entrée
+  prev = millis();               //Mémoriser le compteur de temps actuel
+}
+void clignote(){
+  prev = millis();
+  setWhite();
+}
+float readTemp(){
+  return analogRead(sensorPin);
+}
+void dispatch(){
+  if (temperatureC <= 100)
+  {
+    setVert();
+  }
+  else if (temperatureC>100 && temperatureC<400)
+  {
+    setOrange();
+  }
+  else if (temperatureC >= 400)
+  {
+    setRed();
+  }
+
+//Interrogation de l’état du bouton-poussoir
+  buttonState = digitalRead (buttonPin);
+  if(buttonState == HIGH){
+    Serial.println (readTemp()) ;
+  }
+}
+
+void loop()
 {
-float reading = analogRead(sensorPin); 
-float temperatureC = reading; 
+
+  temperatureC = readTemp();
 // float temperatureC = ((reading*5/1023)-0.5)*100;    // mesure de la température en degré celcius
 //Serial.println (Volt);
 //Serial.println (temperatureC);
-if (temperatureC <= 100)
-{  
-  digitalWrite(ledPinVert, HIGH);
-    digitalWrite(ledPinOrange, LOW);
-    digitalWrite (ledPinRouge, LOW);
-}
-else if (temperatureC>100 && temperatureC<400)
-{
-  if((millis() - prev) > interval)
-{    
-  prev = millis();    
-ledState = !ledState; 
-  }
-  digitalWrite(ledPinVert, LOW);
-  digitalWrite (ledPinRouge, LOW);
-  digitalWrite(ledPinOrange, ledState);
-}
-  else if (temperatureC >= 400)
-  {
-if((millis() - prev) > interval)
-{    
-  prev = millis();    
-ledState = !ledState; 
-}
-  digitalWrite(ledPinRouge, ledState); //Bascule la LED rouge }   
-  digitalWrite(ledPinVert,LOW);
-    digitalWrite(ledPinOrange,LOW);
-    digitalWrite(ledPinVert, LOW);
-  }
-//Interrogation de l’état du bouton-poussoir   
-buttonState = digitalRead (buttonPin);   
+  dispatch();
 
-if(buttonState == HIGH)     
-  {int reading = analogRead(sensorPin);
-  Serial.println (temperatureC) ;
+  if((millis() - prev) > interval)
+  {
+    clignote();
   }
 }
